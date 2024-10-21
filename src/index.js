355b54239059c02e156d28bed2c8689be8107b48
Iryna Oliinyk <arairishka2@gmail.com>
2024-10-21 17:07:52 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,22 +4,32 @@ const app = express();
 
 const PORT = 3000;
 
-// app.get('/', (req, res) => {
-//   res.json({
-//     message: 'I love you!',
-//   });
-// });
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'I love you!',
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is runing on ${PORT}`);
 });
 
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Not Found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
 });
 
 // app.use((req, res, next) => {
-//   console.log(`Time ${new Date().toLocaleString}`);
+//   console.log(`Time: ${new Date().toLocaleString()}`);
 //   next();
 // });
