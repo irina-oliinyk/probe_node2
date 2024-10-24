7feb0ef58297ad9e2bd63cc2e6f002eafe30798b
Iryna Oliinyk <arairishka2@gmail.com>
2024-10-24 18:38:58 +0200

    files

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-import express from 'express';
+import { startServer } from './server.js';
 
-const app = express();
-
-const PORT = 3000;
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'I love you!',
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is runing on ${PORT}`);
-});
-
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Not Found',
-  });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
-
-// app.use((req, res, next) => {
-//   console.log(`Time: ${new Date().toLocaleString()}`);
-//   next();
-// });
+startServer();
